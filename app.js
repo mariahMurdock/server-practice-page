5b530d2e5497d5a6a3eb4faded4dd54743c5133c
mariahMurdock <mariahmurdock1227@gmail.com>
2020-10-23 11:45:14 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// INCLUDE THE EXPRESS PACKAGE IN YOUR CODE
+const express = require('express');
+// WRITE THE CODE TO START THE SERVER
+const app = express();
+// SET VARIABLE TO PORT NUMBER
+const port = 8000;
+// UTILIZE THE .LISTEN METHOD
+const server = app.listen(port, listening);
+// CHECK THE TERMINAL (BY STARTING THE CODE( NODE NAME OF FILE.JS) FOR CONFIRMATION THE CODE IS WORKING OR ANY ERROR MESSAGES FOR ISSUES WITH THE CODE
+function listening(){
+     console.log("server running");
+     console.log(`running on localhost: {$port}`);
+}
---
+++ notes.js
@@ -0,0 +1,9 @@
+// USING NODE
+const bodyParser = require('body-parser')
+
+// EXPRESS OVERVIEW
+// Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
